ff816a425b3d03d1a29de9af572a59c2f97de307
MohanKumar Ramasamy <123152359+MohanKumarR13@users.noreply.github.com>
2024-09-25 16:26:28 +0530

    Update findDuplicateChar.java

--- findDuplicateChar.java
+++ findDuplicateChar.java
@@ -15,3 +15,26 @@ public class findDuplicateChar {
       }
    }
 }
+
+=============================================***************************************************************=========================================================================
+	public class Main
+{
+    
+public static void main(String args[]) {
+    char Char;
+    int count;
+    String a = "Mohan";
+    a = a.toLowerCase();
+    for (Char = 'a'; Char <= 'z'; Char++) {
+        count = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) == Char) {
+                count++;
+            }
+        }
+        System.out.println("Number of occurences of " + Char + " is " + count);
+    }
+    
+    
+}
+}
