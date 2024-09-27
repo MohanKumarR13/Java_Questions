00fd2c2af7c5cb3507a23a74c42e219d80ccd544
MohanKumar Ramasamy <123152359+MohanKumarR13@users.noreply.github.com>
2024-09-27 21:32:09 +0530

    Update findDuplicateChar.java

--- findDuplicateChar.java
+++ findDuplicateChar.java
@@ -17,7 +17,7 @@ public class findDuplicateChar {
 }
 
 =============================================***************************************************************=========================================================================
-	public class Main
+	public class NoOfCharOccurance
 {
     
 public static void main(String args[]) {
