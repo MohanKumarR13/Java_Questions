599db1878970a79b24deefbf0ebbb691d9cfd3cd
MohanKumar Ramasamy <123152359+MohanKumarR13@users.noreply.github.com>
2024-09-27 21:40:06 +0530

    Update findDuplicateChar.java

--- findDuplicateChar.java
+++ findDuplicateChar.java
@@ -43,21 +43,19 @@ public static void main(String args[]) {
 public class PrintUniqueElement
 {
 public static void main(String []args) {
-      int[] my_array = {1,2,3,4,23,1,2,3,4};
+     int[] my_array = {1, 2, 5, 5, 6, 6, 7, 2};
 
-      for (int i = 0; i < my_array.length-1; i++)
-         {
-          boolean is_non_dupe = true;
-          for (int j = 0; j < my_array.length; j++)
-            {
-             if ((my_array[i] == my_array[j]) && (i!=j))
-              {
-                is_non_dupe = false;
-              }
-           }
-           if(is_non_dupe){
-            System.out.println(my_array[i]); 
-           }
-     }
-  } 
+     for (int i = 0; i < my_array.length; i++) {
+     boolean dup = false;
+     for (int j = 0; j < my_array.length; j++) {
+        if (i != j && my_array[i] == my_array[j]) {
+            dup = true;
+            break;
+        }
+    }
+    if (! dup) {
+        System.out.println(my_array[i]);
+    }
+}
+}
 }
