db62fc56c11b1c3582b07f955d172c013c0a1613
MohanKumar Ramasamy <123152359+MohanKumarR13@users.noreply.github.com>
2024-09-27 21:31:14 +0530

    Update findDuplicateChar.java

--- findDuplicateChar.java
+++ findDuplicateChar.java
@@ -38,3 +38,26 @@ public static void main(String args[]) {
     
 }
 }
+===============================================================================================================================================================
+	import java.util.*;
+public class PrintUniqueElement
+{
+public static void main(String []args) {
+      int[] my_array = {1,2,3,4,23,1,2,3,4};
+
+      for (int i = 0; i < my_array.length-1; i++)
+         {
+          boolean is_non_dupe = true;
+          for (int j = 0; j < my_array.length; j++)
+            {
+             if ((my_array[i] == my_array[j]) && (i!=j))
+              {
+                is_non_dupe = false;
+              }
+           }
+           if(is_non_dupe){
+            System.out.println(my_array[i]); 
+           }
+     }
+  } 
+}
